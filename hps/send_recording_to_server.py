c6fa65949a05076932762c70ce75c896a0020547
Patrick Creighton <pcreighton429@gmail.com>
2022-03-24 16:26:18 -0700

    Move constants to the top and update documentation

--- hps/send_recording_to_server.py
+++ hps/send_recording_to_server.py
@@ -1,4 +1,4 @@
-''' Records audio for 5 seconds then:
+''' Records the left channel of mic-in for RECORDING_LENGTH seconds then:
     - Plays it back if PLAY_BACK_AUDIO is True
     - Posts wave_data as a JSON object to SERVER_ENDPOINT
 '''
@@ -10,8 +10,10 @@ import sys
 
 AUDIO_TO_WAV_DIVIDER = 2**12 - 1
 PLAY_BACK_AUDIO = True
+RECORDING_LENGTH = 5
 SAMPLING_RATE = audio.MIN_SAMPLING_RATE  # 8000 Hz
 SERVER_ENDPOINT = 'http://34.222.245.107:4000/api/audio'
+VOLUME_MULTIPLIER = 42
 
 if not audio.open_dev():
     sys.exit()
@@ -21,17 +23,16 @@ print('Recording audio')
 wave_data = []
 audio.init()
 audio.sampling_rate(SAMPLING_RATE)
-for _ in range(SAMPLING_RATE * 5):
+for _ in range(SAMPLING_RATE * RECORDING_LENGTH):
     audio.wait_read()
     left, _ = audio.read()
     wave_data.append(left / AUDIO_TO_WAV_DIVIDER)
 
 if PLAY_BACK_AUDIO:
     print('Playing back audio')
-    volume_multiplier = 42
     for data in wave_data:
         audio.wait_write()
-        left = data * AUDIO_TO_WAV_DIVIDER * volume_multiplier
+        left = data * AUDIO_TO_WAV_DIVIDER * VOLUME_MULTIPLIER
         if left > audio.MAX_VOLUME:
             print('WARN: Max volume exceeded, writing audio.MAX_VOLUME')
             left = audio.MAX_VOLUME
@@ -43,4 +44,4 @@ audio.close()
 print('Posting wave_data')
 requests.post(SERVER_ENDPOINT, data=json.dumps(wave_data))
 
-print('Exiting program')
+print('Finished program, exiting...')
