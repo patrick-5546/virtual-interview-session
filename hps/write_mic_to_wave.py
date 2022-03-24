f3997fcb2e91df0bb0b1c149f66ce55249fd717c
azwadsadman <50250714+azwadsadman@users.noreply.github.com>
2022-03-24 13:09:43 -0700

    updated the audio script

--- hps/write_mic_to_wave.py
+++ hps/write_mic_to_wave.py
@@ -1,22 +1,33 @@
 ''' Records audio for 5 seconds and plays it back '''
 import sys
 import audio
+import wave
+import struct
+from numpy.fft import fft, ifft
+#import matplotlib.pyplot as plt
 
 if not audio.open_dev():
     sys.exit()
 audio.init()
-sampling_rate = audio.MIN_SAMPLING_RATE
+out = wave.open('output.wav','w')
+out.setnchannels(1)
+out.setsampwidth(2)
+
+sampling_rate = 8000   #audio.MIN_SAMPLING_RATE
 print(sampling_rate)
 audio.sampling_rate(sampling_rate)
+out.setframerate(sampling_rate)
 
 print('Recording audio')
 channels = []
 for _ in range(sampling_rate * 5):
     audio.wait_read()
-    channels.append(audio.read())
+    left, right = audio.read()
+    out.writeframesraw(struct.pack('<h', left/((2**12)-1)))
+    channels.append((left, right))
 
 print('Playing back audio')
-volume_multiplier = 32
+volume_multiplier = 42
 max_volume = -1
 for left, right in channels:
     audio.wait_write()
@@ -30,5 +41,7 @@ for left, right in channels:
     audio.write_left(volume_multiplier * left)
     audio.write_right(volume_multiplier * right)
 
-audio.close()
+out.close()
+audio.close() 
 print("Exiting program, max volume={}\n".format(max_volume))
+
