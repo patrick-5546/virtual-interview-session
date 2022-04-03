020b035f0dd9774e0d0a1705a586397d5012b443
chrischang5 <1chang1chang1@gmail.com>
2022-04-02 20:46:04 -0700

    Update readdata assignments

--- fpga/image_reader/image_reader.sv
+++ fpga/image_reader/image_reader.sv
@@ -42,15 +42,7 @@ module image_reader (
             saved_value <= writedata; // store result
     end
 
-    // must check that x and y are within the bounding box.
-    // at a certain x, y you can start registering MCUs and sending them off to the DCT instances.
-    // Then when we reach the last pixel of the strip, stop constructing MCUs and reset your counters.
-
-     /* Bounding box around the 224x224 capture field centered on the 640x480 screen
-            - Top left coordinates of capture field:     (208, 128) 
-            - Bottom right coordinates of capture field: (432, 352) */
-
-    // MCUs Construction FSM, note we need a "strip number", which is contained in the write data that will dictate when we start constructing MCUs
+    //------MCU Construction FSM-
 
     logic [7:0][7:0][31:0] mcu0;
     logic [7:0][7:0][31:0] mcu1;
@@ -118,6 +110,12 @@ module image_reader (
 
     logic [10:0] strip_x = 0;
     logic [10:0] strip_y = 0;
+
+    /* Bounding box around the 224x224 capture field centered on the 640x480 screen
+            - Top left coordinates of capture field:     (208, 128) 
+            - Bottom right coordinates of capture field: (432, 352) */
+
+    // Check X and Y are within the strip indicated by the strip number
     
     wire reading_from_bounding_box = ((208 <= X) && (X <= 432) && ((127 + 8 * strip_number) <= Y) && (Y <= (127 + 8 * (strip_number + 1))));
     wire last_pixel = (X == 432) && (Y == (127 + 8 * (strip_number + 1)));
@@ -132,7 +130,7 @@ module image_reader (
                 strip_y <= strip_y + 1;
             end
         end
-        // If we reach the last pixel of the strip, reset both x and y.
+        // If we reach the last pixel of the strip, reset both strip_x and strip_y for next time.
         if(last_pixel) begin
             strip_x <= 0;
             strip_y <= 0;
@@ -176,22 +174,84 @@ module image_reader (
     logic [7:0][7:0][31:0] dct_out;
     dct_quantization dct(.mcu_in(dct_input), .dct_out(dct_out));
 
-    logic [2047:0] flattened_dct_out;
+    // logic [2047:0] flattened_dct_out;
 
-    flatten3d flat(.in_array(dct_out), .flatten_out(flattened_dct_out));
+    // flatten3d flat(.in_array(dct_out), .flatten_out(flattened_dct_out));
 
-    logic [1023:0] first_half = flattened_dct_out[1023:0];
-    logic [1023:0] second_half = flattened_dct_out[2047:1024];
+    // logic [1023:0] first_half = flattened_dct_out[1023:0];
+    // logic [1023:0] second_half = flattened_dct_out[2047:1024];
 
     // reading
     always @(*) begin
         readdata <= 32'b0; // default value is 0
         if (rd_en == 1)
-            if ( addr == 2'b00 )
-                readdata <= first_half;
-            else
-                readdata <= second_half; // what was written
-
+            case(addr)
+                0: readdata <= dct_out[0][0];
+                1: readdata <= dct_out[0][1];
+                2: readdata <= dct_out[0][2];
+                3: readdata <= dct_out[0][3];
+                4: readdata <= dct_out[0][4];
+                5: readdata <= dct_out[0][5];
+                6: readdata <= dct_out[0][6];
+                7: readdata <= dct_out[0][7];
+                8: readdata <= dct_out[1][0];
+                9: readdata <= dct_out[1][1];
+                10: readdata <= dct_out[1][2];
+                11: readdata <= dct_out[1][3];
+                12: readdata <= dct_out[1][4];
+                13: readdata <= dct_out[1][5];
+                14: readdata <= dct_out[1][6];
+                15: readdata <= dct_out[1][7];
+                16: readdata <= dct_out[2][0];
+                17: readdata <= dct_out[2][1];
+                18: readdata <= dct_out[2][2];
+                19: readdata <= dct_out[2][3];
+                20: readdata <= dct_out[2][4];
+                21: readdata <= dct_out[2][5];
+                22: readdata <= dct_out[2][6];
+                23: readdata <= dct_out[2][7];
+                24: readdata <= dct_out[3][0];
+                25: readdata <= dct_out[3][1];
+                26: readdata <= dct_out[3][2];
+                27: readdata <= dct_out[3][3];
+                28: readdata <= dct_out[3][4];
+                29: readdata <= dct_out[3][5];
+                30: readdata <= dct_out[3][6];
+                31: readdata <= dct_out[3][7];
+                32: readdata <= dct_out[4][0];
+                33: readdata <= dct_out[4][1];
+                34: readdata <= dct_out[4][2];
+                35: readdata <= dct_out[4][3];
+                36: readdata <= dct_out[4][4];
+                37: readdata <= dct_out[4][5];
+                38: readdata <= dct_out[4][6];
+                39: readdata <= dct_out[4][7];
+                40: readdata <= dct_out[5][0];
+                41: readdata <= dct_out[5][1];
+                42: readdata <= dct_out[5][2];
+                43: readdata <= dct_out[5][3];
+                44: readdata <= dct_out[5][4];
+                45: readdata <= dct_out[5][5];
+                46: readdata <= dct_out[5][6];
+                47: readdata <= dct_out[5][7];
+                48: readdata <= dct_out[6][0];
+                49: readdata <= dct_out[6][1];
+                50: readdata <= dct_out[6][2];
+                51: readdata <= dct_out[6][3];
+                52: readdata <= dct_out[6][4];
+                53: readdata <= dct_out[6][5];
+                54: readdata <= dct_out[6][6];
+                55: readdata <= dct_out[6][7];
+                56: readdata <= dct_out[7][0];
+                57: readdata <= dct_out[7][1];
+                58: readdata <= dct_out[7][2];
+                59: readdata <= dct_out[7][3];
+                60: readdata <= dct_out[7][4];
+                61: readdata <= dct_out[7][5];
+                62: readdata <= dct_out[7][6];
+                63: readdata <= dct_out[7][7];
+                default: readdata <= 1'bx;
+            endcase
     end
 
 endmodule
