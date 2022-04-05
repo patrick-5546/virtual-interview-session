a15aa59949cf397422c919f0b472e600e02caf9f
azwadsadman <50250714+azwadsadman@users.noreply.github.com>
2022-04-05 00:37:13 -0700

    feedback not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-sequences */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import axios from "axios";
 import LoginForm from "./components/LoginForm";
 import Home from "./components/Home";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Metrics from "./routes/Metrics";
 import Interview from "./routes/Interview";
+import Questions from "./routes/Questions";
+import DisplayComponent from './components/DisplayComponent';
+import BtnComponent from './components/BtnComponent';
 
 function App() {
   const adminUser = {
@@ -18,15 +21,37 @@ function App() {
   const [sentiment_score, setSentimentscore] = useState("");
   const [user, setUser] = useState({ firstname: "", lastname: "", email: "" });
   const [error, setError] = useState("");
+  const [question, setQuestion] = useState("");
+  const [question2, setQuestion2] = useState("");
+  const [question3, setQuestion3] = useState("");
+  
+  var data2 = {
+    question: question
+  }
+  var data3 = {
+    question: question2
+  }
+  var data4 = {
+    question: question3
+  }
 
+  /*
   var data = {
     eyecontact: eyecontact,
     sentiment_score: sentiment_score
   }
 
+  const updatedata = (eyecontact, sentiment_score) => {
+    setEyecontact(eyecontact);
+    setSentimentscore(sentiment_score);
+  }
+  */
+  
+  /*
+  //const update = () => {
   useEffect(() => {
     axios
-      .get(`${APIUrl}/feedback`)
+      .get(`${APIUrl}/feedback1`)
       .then(
         (response) => {
           setEyecontact(response.data.feedback);
@@ -40,7 +65,7 @@ function App() {
       });
     
       axios
-      .get(`${APIUrl}/feedback`)
+      .get(`${APIUrl}/feedback2`)
       .then(
         (response) => {
           setSentimentscore(response.data.feedback);
@@ -51,6 +76,52 @@ function App() {
       )
       .catch((err) => {
         console.log(err);
+      });
+    */
+    //useEffect(update, []);
+    
+
+    useEffect(() => {  
+      axios
+      .get(`${APIUrl}/c`)
+      .then(
+        (response) => {
+          setQuestion([response.data[0].q, response.data[1].q, response.data[2].q, response.data[3].q, response.data[4].q]);
+        },
+        (reject) => {
+          console.log(reject);
+        }
+      )
+      .catch((err) => {
+        console.error(err);
+      });
+      
+      axios
+      .get(`${APIUrl}/cplus`)
+      .then(
+        (response) => {
+          setQuestion2([response.data[0].q, response.data[1].q, response.data[2].q, response.data[3].q, response.data[4].q]);
+        },
+        (reject) => {
+          console.log(reject);
+        }
+      )
+      .catch((err) => {
+        console.error(err);
+      });
+
+      axios
+      .get(`${APIUrl}/javascript`)
+      .then(
+        (response) => {
+          setQuestion3([response.data[0].q, response.data[1].q, response.data[2].q, response.data[3].q, response.data[4].q]);
+        },
+        (reject) => {
+          console.log(reject);
+        }
+      )
+      .catch((err) => {
+        console.error(err);
       });
   }, []);
 
@@ -77,23 +148,62 @@ function App() {
     setUser({ firstname: "", lastname: "", email: "" });
   };
 
+  
+  // STATE to store data to pass to Metrics Component (useState = metrics)
+  // callback wrapper function to set the state metrics state 
+  const [metrics, setMetrics] = useState({}); 
+
+  const updateMetrics = (metrics) => {
+    setMetrics(metrics);
+  }
+
+
   return (
     <Router>
       <div className="App">
         {user.email !== "" ? (
           <div className="welcome">
-            <Home user={user} Logout={Logout}></Home>
+            <Home user={user} Logout={Logout} updateMetricsProps={updateMetrics}></Home>
             <Routes>
               <Route path="/interview" element={<Interview />}></Route>
               <Route
                 path="/feedback"
                 element={
                   <Metrics
-                    data={data}
+                    data={metrics}
+                  />
+                }
+              >
+                
+              </Route>
+            <Route path="/interview" element={<Interview />}></Route>
+              <Route
+                path="/cquestion"
+                element={
+                  <Questions
+                    data={data2}
                   />
                 }
               ></Route>
-            </Routes>
+              <Route path="/interview" element={<Interview />}></Route>
+              <Route
+                path="/cplusquestion"
+                element={
+                  <Questions
+                    data={data3}
+                  />
+                }
+              ></Route>
+              <Route path="/interview" element={<Interview />}></Route>
+              <Route
+                path="/jsquestion"
+                element={
+                  <Questions
+                    data={data4}
+                  />
+                }
+              ></Route>
+              </Routes>
           </div>
         ) : (
           <Routes>
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,8 +1,173 @@
-import React from "react";
-import {Link } from "react-router-dom";
+import React, {useState, useEffect, Component} from "react";
+import {Link, useNavigate, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import axios from "axios";
+import Box from '@material-ui/core/Box';
+import { Button, ButtonGroup } from "@material-ui/core";
+import DisplayComponent from './DisplayComponent';
+import BtnComponent from './BtnComponent';
 
-function Home({ user, Logout }) {
+import Metrics from "../routes/Metrics";
+import Interview from "../routes/Interview";
+import Questions from "../routes/Questions";
 
+function Home({ user, Logout, updateMetricsProps }) {
+  /*
+  const APIUrl = "http://34.222.245.107:5000/api";
+  const [question, setQuestion] = useState("");
+  var data = {
+    question: question
+  }
+  useEffect(() => {
+    axios
+      .get(`${APIUrl}/c`)
+      .then(
+        (response) => {
+          setQuestion(response.data[0].q);
+        },
+        (reject) => {
+          console.log(reject);
+        }
+      )
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
+  
+  
+
+  if(question.error) {
+    content = <p> There was an error </p>
+  }
+
+  if(question.data) {
+    content  = <div>
+      <h1>No question</h1>
+    </div>
+  }
+  */
+
+  //Timer//
+  const [time, setTime] = useState({ms:0, s:0, m:0});
+  const [interv, setInterv] = useState();
+  const [status, setStatus] = useState(0);
+  // Not started = 0
+  // started = 1
+  // stopped = 2
+
+  const start = () => {
+    run();
+    setStatus(1);
+    setInterv(setInterval(run, 10));
+  };
+
+  var updatedMs = time.ms, updatedS = time.s, updatedM = time.m;
+
+  const run = () => {
+    if(updatedM === 60){
+      //updatedH++;
+      if(updatedM === 5) {
+        setStatus(2);
+      }
+    }
+    if(updatedS === 60){
+      updatedM++;
+      updatedS = 0;
+    }
+    if(updatedMs === 100){
+      updatedS++;
+      updatedMs = 0;
+    }
+    updatedMs++;
+    return setTime({ms:updatedMs, s:updatedS, m:updatedM});
+  };
+
+  const stop = () => {
+    clearInterval(interv);
+    setStatus(2);
+  };
+
+  const reset = () => {
+    clearInterval(interv);
+    setStatus(0);
+    setTime({ms:0, s:0, m:0, h:0})
+  };
+
+  const resume = () => start();
+
+
+  let navigate = useNavigate(); 
+  const routeChangeC = () =>{ 
+    let path = `/cquestion`; 
+    navigate(path);
+  }
+
+  const routeChangeCplus = () =>{ 
+    let path = `/cplusquestion`; 
+    navigate(path);
+  }
+
+  const routeChangejs = () =>{ 
+    let path = `/jsquestion`; 
+    navigate(path);
+  }
+
+  const [show, setShow] = useState(false);
+    const handleOnClick = () => {
+        setShow(true);
+    }
+  
+  const [eyecontact, setEyecontact] = useState("");
+  const [sentiment_score, setSentimentscore] = useState("");
+
+  
+  var data = {
+    eyecontact: eyecontact,
+    sentiment_score: sentiment_score
+  }
+
+  const updatedata = (eyecontact, sentiment_score) => {
+    setEyecontact(eyecontact);
+    setSentimentscore(sentiment_score);
+  }
+  
+    
+  const APIUrl = "http://34.222.245.107:5000/api";
+    
+    
+    const update = () => {
+      //useEffect(() => {
+        axios
+          .get(`${APIUrl}/feedback1`)
+          .then(
+            (response) => {
+              setEyecontact(response.data.feedback);
+            },
+            (reject) => {
+              console.log(reject);
+            }
+          )
+          .catch((err) => {
+            console.error(err);
+          });
+        
+          axios
+          .get(`${APIUrl}/feedback2`)
+          .then(
+            (response) => {
+              setSentimentscore(response.data.feedback)
+              updateMetricsProps(data)
+            },
+            (reject) => {
+              console.log(reject);
+            }
+          )
+          .catch((err) => {
+            console.log(err);
+          });
+          
+          console.log(data)
+        }
+  
   return (
     <div>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
@@ -10,14 +175,43 @@ function Home({ user, Logout }) {
           <h2>
             Welcome, <span>{user.firstname}</span>
           </h2>
-          <button type="button" aria-expanded="false">
+          <button type="button" aria-expanded="false" onClick={update}>
             <Link to="/feedback"><span>Feedback </span> </Link>
           </button>
         </div>
-
         <button onClick={Logout}> <Link to="">Logout</Link></button>
       </nav>
-
+      <div>
+        <h1>
+          Please select an option to start the interview!
+        </h1>
+      </div>
+      <div style={{ marginLeft: '40%', marginTop: '60px' }}></div>
+      <ButtonGroup aria-label="outlined primary button group">
+      <Box color="black" borderRadius="5px" height="80px" width="150px" bgcolor="orange" p="10px" cursor="pointer" display="flex" justifyContent="center" alignItems="center" fontWeight="bold" style={{cursor:'pointer'}} onClick={() => {routeChangeC(); handleOnClick();}}>
+        C 
+      </Box>
+     </ButtonGroup>
+     <ButtonGroup orientation="horizontal" aria-label="outlined primary button group">
+     <Box marginLeft="40px" borderRadius="5px" height="80px" width="150px" color="black" bgcolor="orange" p="10px" display="flex" justifyContent="center" alignItems="center" fontWeight="bold" style={{cursor:'pointer'}} onClick={() => {routeChangeCplus(); handleOnClick();}}>
+        C++ 
+      </Box>
+      </ButtonGroup>
+      <ButtonGroup orientation="horizontal" aria-label="outlined primary button group">
+     <Box marginLeft="40px" borderRadius="5px" height="80px" width="150px" color="black" bgcolor="orange" p="10px" display="flex" justifyContent="center" alignItems="center" fontWeight="bold" style={{cursor:'pointer'}} onClick={() => {routeChangejs(); handleOnClick();}}>
+        JavaScript 
+      </Box>
+      </ButtonGroup>
+      <div>
+        {show && (<div className="main-section">
+            <div className="clock-holder">
+            <div className="stopwatch">
+               <DisplayComponent time={time}/>
+               <BtnComponent status={status} resume={resume} reset={reset} stop={stop} start={start}/>
+          </div>
+          </div>
+          </div>)}
+      </div>
     </div>
   );
 }
